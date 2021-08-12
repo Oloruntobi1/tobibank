6dab2f56c6abd1b746e47eccb9848271fa0d364a
Oloruntobi1 <blgntobi@gmail.com>
2021-08-12 23:47:57 +0100

    changes

--- tobank.go
+++ tobank.go
@@ -1,6 +1,6 @@
 package tobibank
 
-import "github.com/Oloruntobi1/payload"
+//import "github.com/Oloruntobi1/payload"
 
 type TobiBank struct {
 	ApiKey string
@@ -14,7 +14,11 @@ func(w *TobiBank) Create() string {
 	return "Created by Wallet africa"
 }
 
-func(m *TobiBank) Payout() payload.WalletPayoutResponse{
-	return payload.WalletPayoutResponse{}
+// func(m *TobiBank) Payout() payload.WalletPayoutResponse{
+// 	return payload.WalletPayoutResponse{}
+// }
+
+func(m *TobiBank) Payout() string{
+	return "my head oo"
 }
 
